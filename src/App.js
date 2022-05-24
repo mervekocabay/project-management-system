6a048d382f323b92a529ab39b1a3055d961f1701
mervekocabay <kocabayymerve@gmail.com>
2022-05-24 20:24:43 +0300

    Genel listetasarımı yapıldı

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import List from 'components/List'
 import Navbar from 'components/Navbar'
 import React from 'react'
 
 const App = () => {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <List />
+    </>
   )
 }
 
---
+++ src/components/List.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const List = () => {
+    const array = ["a", "a", "a", "a"]
+
+    return (
+        <div className='min-h-screen flex'>
+            <div className='flex-1 max-w-4x1 max-auto p-5'>
+                <ul className='grid grid-cols-5 gap-2'>
+                    {
+                        array?.map((item, index) => (
+                            <li key={index} className='bg-gray-200 rounded-lg shadow-xl mt-3'>
+                                <div className='p-3'>
+                                    <h1 className='capitalize font-bold'>
+                                        Title
+                                    </h1 >
+                                </div>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default List
