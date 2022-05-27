3ae7dbd22bf7c7576764f82838f9fbca8b2ff7c3
mervekocabay <kocabayymerve@gmail.com>
2022-05-27 15:34:54 +0300

    Liste silme işlemi tamamlandı

--- src/components/AddCard.js
+++ src/components/AddCard.js
@@ -20,7 +20,7 @@ const AddCard = ({ setCardData, cardData, listId }) => {
                 <Plus size={15} /> Yeni Kart Ekle
             </div>
             :
-            <div className="bg-gray-200 pl-3 pr-3 rounded-lg shadow-xl h-28 cursor-pointer font-semibold text-xs">
+            <div className=" h-28 cursor-pointer font-semibold text-xs">
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <textarea {...register("description", { required: true })} className="px-3 py-3 rounded-md placeholder-slate-300 h-12 text-slate-600 relative bg-white text-sm border-0 shadow outline-none m-auto focus:outline-none focus:ring w-full" />
                     <div className='flex  mt-2 items-center'>
--- src/components/List.js
+++ src/components/List.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
+import { Edit, Trash } from 'react-feather'
 import AddCard from './AddCard'
 import AddList from './AddList'
 import Card from './Card'
@@ -9,6 +10,8 @@ const List = () => {
 
     const [listData, setlistData] = useState([])
     const [cardData, setCardData] = useState([])
+    const [hover, setHover] = useState({ status: false, listId: null })
+
 
     const handleDragEnd = (result) => {
         if (!result.destination) return
@@ -17,6 +20,18 @@ const List = () => {
         items.splice(result.destination.index, 0, removedItem)
         setlistData(items)
     }
+
+    const removeItem = (item) => {
+        //Liste Silindiyse o listeye bağlı olan kartlarında silinmesi gerekir.
+        setlistData((prevState) =>
+            prevState.filter((prevItem) => prevItem.listId !== item)
+        );
+        setCardData((prevState) =>
+            prevState.filter((prevItem) => prevItem.listId !== item)
+        )
+    };
+
+
 
     return (
         <div className='min-h-screen flex'>
@@ -30,13 +45,25 @@ const List = () => {
                                         {
                                             (provider) => (
                                                 <li ref={provider.innerRef} {...provider.draggableProps} {...provider.dragHandleProps} className='bg-gray-200 h-fit rounded-lg shadow-xl mt-3'>
-                                                    <div className='p-3'>
+                                                    <div
+                                                        onMouseEnter={() => setHover({ status: true, listId: item.listId })}
+                                                        onMouseLeave={() => setHover({ status: false, listId: item.listId })}
+                                                        className='flex relative p-3'>
                                                         <h1 className='capitalize font-bold'>
                                                             {item.title}
                                                         </h1 >
-                                                        <Card listData={listData} cardData={cardData} setCardData={setCardData} listId={item.listId} />
+                                                        {(hover.status && hover.listId === item.listId) &&
+                                                            <>
+                                                                <Edit size={20} className='float-right right-4 hover:bg-gray-300 absolute text-blue-400' />
+                                                                <Trash size={20} onClick={() => removeItem(item.listId)} className='float-right right-10 absolute hover:bg-gray-300 text-red-400' />
+                                                            </>
+                                                        }
                                                     </div>
-                                                    <AddCard setCardData={setCardData} cardData={cardData} listId={item.listId} />
+
+                                                    <div className='p-3'>
+                                                        <Card listData={listData} cardData={cardData} setCardData={setCardData} listId={item.listId} />
+                                                        <AddCard setCardData={setCardData} cardData={cardData} listId={item.listId} />
+                                                    </div>
                                                 </li>
                                             )
                                         }
